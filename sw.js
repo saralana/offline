157988a225f8c64bf1aad1826af0a5e167a0a25f
Sara Lana <saralgc@gmail.com>
2025-07-10 11:59:41 +0200

    first comiit
    trying to download files for being used offline

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'audio-cache';
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => {
+      return resp || fetch(event.request);
+    })
+  );
+});
